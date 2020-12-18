ebc40052886b86bfa06241fd6ca5a1b26f49b7c4
koke <jorge.arancibia.jeal@gmail.com>
2020-12-18 13:36:17 -0300

    build working right

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 Una vez dentro del directorio, se puede:
 
-### `npm start` 
+### `npm start`
+## Para correr el proyecto de forma local.
 
-## Para correr el proyecto en ambiente de desarrollo.
 
-### `npm run build` 
-
+### `npm run build`
 ## Para generar el build que muestra el backend.
+
+## Si se quiere visualizar los últimos cambios hechos en el front se debe hacer lo siguiente:
+# Lo que genera la carpeta build, se debe copiar y pegar en la carpeta 'public' del backend
+
+### En este caso, Este proceso ya viene listo con los últimos cambios.
--- src/helpers/getInfo.js
+++ src/helpers/getInfo.js
@@ -1,9 +1,17 @@
+const environment = {
+  dev: "http://localhost:4000/",
+  prod: "https://back-ripley.herokuapp.com/",
+};
+
 export const getInfo = async () => {
-  const url = `${process.env.ENVIRONMENT}/api/productos`
-  const response = await fetch(url);
-  const data = await response.json();
+	
+	const HOST = environment.dev;
 
-  // const result = data.map(object => object)
+	const url = `${HOST}api/productos`;
+	const response = await fetch(url);
+	const data = await response.json();
 
-  return data
-}
+	// const result = data.map(object => object)
+
+	return data;
+};
